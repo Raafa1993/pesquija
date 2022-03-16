41f1a60c4020a5a32af5ed4a16fdeed55bdf2942
Raafa1993 <rafaelajds@gmail.com>
2022-03-16 00:52:14 -0300

    update question

--- src/icons/PauseIcon.tsx
+++ src/icons/PauseIcon.tsx
@@ -1,6 +1,12 @@
 export const PauseIcon: React.FC = () => (
-    <svg width="8" height="32" viewBox="0 0 8 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M0 0H8V32H0V0.000147403V0Z" fill="black" />
-    </svg>
-
-)
+  <svg
+    width="32"
+    height="32"
+    viewBox="0 0 32 32"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect width="32" height="32" rx="16" fill="#089BFF" />
+    <path d="M12 11H15V21H12V11ZM17 11H20V21H17V11Z" fill="white" />
+  </svg>
+);
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.15.8:3332',
-  // baseURL: 'https://pesquija.backendtropa.com.br/',
+  // baseURL: 'http://192.168.15.8:3332',
+  baseURL: 'https://pesquija.backendtropa.com.br/',
 });
 
 export default api;
