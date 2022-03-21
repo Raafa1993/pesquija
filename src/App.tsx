e7d476130b2d3472ed1f5de00f9ada6fb1a69ed0
Fino59 <slipdan@hotmail.com>
2022-03-21 15:23:08 -0300

    Update with Toaster for errors

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./hooks/Auth";
 import Routes from "./routes";
 import { GlobalStyles } from "./styles/global";
+import { ToastProvider } from 'react-toast-notifications';
 
 function App() {
   return (
     <>
       <BrowserRouter>
           <AuthProvider>
-            <Routes />
+            <ToastProvider>
+              <Routes />
+            </ToastProvider>
           </AuthProvider>
       </BrowserRouter>
       <GlobalStyles />
